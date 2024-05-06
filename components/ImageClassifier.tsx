cb03c34f769d78dc2735d90ed5e6a534656e5048
Shreyak Chakraborty <shreyak.rekshda@gmail.com>
2024-05-06 20:24:15 +0530

    add image classifier component (WIP)

---
+++ components/ImageClassifier.tsx
@@ -0,0 +1,9 @@
+interface Props {
+    height: number;
+    width: number;
+  }
+
+// Run image classification inference on an uploaded image within browser
+const ImageClassifier = (props: Props) => {
+  
+}
